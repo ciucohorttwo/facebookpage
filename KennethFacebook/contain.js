815b07c825a68a7a18307e7c3f9fed40bf42326d
OcittiKennenth <kennethocitti95@gmail.com>
2019-08-08 17:23:22 +0300

    first push

---
+++ KennethFacebook/contain.js
@@ -0,0 +1,64 @@
+// const todos = [
+
+//     {
+//         id: 1,
+//         text: 'This is ID one',
+//         completed: true
+//     },
+//     {
+//         id: 2,
+//         text: 'This is ID two',
+//         completed: true
+//     },
+//     {
+//         id: 3,
+//         text: 'This is ID three',
+//         completed: true
+//     }
+// ];
+// const todosJSON = JSON.stringify(todos);
+
+// for (let ken of todos) {
+//     console.log(ken.text)
+// }
+// todos.forEach(function(car) {
+
+// // })
+
+// function Person(firstName, lastName, dob) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.dob = new Date(dob);
+//     this.getBirthday = function() {
+//         return this.dob.getFullYear();
+//     }
+//     this.getFullName = function() {
+//         return `${this.firstName} ${this.lastName}`;
+//     }
+// }
+
+// Person.prototype.getBirthday = function() {
+//     return this.dob.getFullYear();
+// }
+
+// class Person {
+//     constructor(firstName, lastName, dob) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.dob = new Date(dob);
+//     }
+//     getBirthday() {
+//         return this.dob.getFullYear();
+//     }
+//     getFullName() {
+//         return this.dob.getFullYear();
+//     }
+// }
+
+
+
+// const Person4 = new Person('kenny', 'ken', '3-4-4353');
+// const Person6 = new Person('steve', 'mandella', '4-3-3324');
+
+// console.log(Person6.getFullName());
+// console.log(Person4);
